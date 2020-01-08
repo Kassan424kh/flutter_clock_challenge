bb7d6861e10ee0ca3145536603cb3a5c5322fdcb
khalil <khalilkhalil@outlook.de>
2020-01-08 21:38:02 +0100

    Added clock numbers 3d

--- digital_clock/lib/digital_clock.dart
+++ digital_clock/lib/digital_clock.dart
@@ -1,34 +1,16 @@
-// Copyright 2019 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
+import 'dart:ui';
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_clock_helper/model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
 import 'package:intl/intl.dart';
+import 'package:vector_math/vector_math_64.dart' show radians;
 
-enum _Element {
-  background,
-  text,
-  shadow,
-}
-
-final _lightTheme = {
-  _Element.background: Color(0xFF81B3FE),
-  _Element.text: Colors.white,
-  _Element.shadow: Colors.black,
-};
-
-final _darkTheme = {
-  _Element.background: Colors.black,
-  _Element.text: Colors.white,
-  _Element.shadow: Color(0xFF174EA6),
-};
-
-/// A basic digital clock.
-///
-/// You can do better than this!
 class DigitalClock extends StatefulWidget {
   const DigitalClock(this.model);
 
@@ -38,9 +20,45 @@ class DigitalClock extends StatefulWidget {
   _DigitalClockState createState() => _DigitalClockState();
 }
 
-class _DigitalClockState extends State<DigitalClock> {
-  DateTime _dateTime = DateTime.now();
+class _DigitalClockState extends State<DigitalClock>
+    with TickerProviderStateMixin {
+  var _now = DateTime.now();
+  var _temperature = '';
+  var _temperatureRange = '';
+  var _condition = '';
+  var _location = '';
+  double radiansPerSeconds = radians(360 / 10);
+  double radiansPerMinutes = radians(360 / 10);
+  double radiansPerHour = radians(360 / 12);
+  double _radians = radians(360 / 24);
+
   Timer _timer;
+  GlobalKey _clockBoxKey = GlobalKey();
+  Size _clockBoxSize = Size(0, 0);
+
+  /// Need using
+  // Offset _clockBoxPosition;
+
+  _getSizes() {
+    final RenderBox renderBoxRed =
+        _clockBoxKey.currentContext.findRenderObject();
+    final sizeRed = renderBoxRed.size;
+    setState(() => _clockBoxSize = sizeRed);
+  }
+
+  /// Need using
+  /*_getPositions() {
+    final RenderBox renderBoxRed = _clockBoxKey.currentContext.findRenderObject();
+    final positionRed = renderBoxRed.localToGlobal(Offset.zero);
+    setState(() => _clockBoxPosition = positionRed);
+  }*/
+
+  _afterLayout(_) {
+    _getSizes();
+
+    /// Need using
+    //_getPositions();
+  }
 
   @override
   void initState() {
@@ -48,11 +66,16 @@ class _DigitalClockState extends State<DigitalClock> {
     widget.model.addListener(_updateModel);
     _updateTime();
     _updateModel();
+
+    WidgetsBinding.instance.addPostFrameCallback(_afterLayout);
   }
 
   @override
   void didUpdateWidget(DigitalClock oldWidget) {
     super.didUpdateWidget(oldWidget);
+
+    _clockBoxSize = MediaQuery.of(context).size;
+
     if (widget.model != oldWidget.model) {
       oldWidget.model.removeListener(_updateModel);
       widget.model.addListener(_updateModel);
@@ -63,71 +86,132 @@ class _DigitalClockState extends State<DigitalClock> {
   void dispose() {
     _timer?.cancel();
     widget.model.removeListener(_updateModel);
-    widget.model.dispose();
     super.dispose();
   }
 
   void _updateModel() {
     setState(() {
-      // Cause the clock to rebuild when the model changes.
+      _temperature = widget.model.temperatureString;
+      _temperatureRange = '(${widget.model.low} - ${widget.model.highString})';
+      _condition = widget.model.weatherString;
+      _location = widget.model.location;
     });
   }
 
   void _updateTime() {
     setState(() {
-      _dateTime = DateTime.now();
-      // Update once per minute. If you want to update every second, use the
-      // following code.
+      _now = DateTime.now();
       _timer = Timer(
-        Duration(minutes: 1) -
-            Duration(seconds: _dateTime.second) -
-            Duration(milliseconds: _dateTime.millisecond),
+        Duration(seconds: 1) - Duration(milliseconds: _now.millisecond),
         _updateTime,
       );
-      // Update once per second, but make sure to do it at the beginning of each
-      // new second, so that the clock is accurate.
-      // _timer = Timer(
-      //   Duration(seconds: 1) - Duration(milliseconds: _dateTime.millisecond),
-      //   _updateTime,
-      // );
     });
+
+    WidgetsBinding.instance.addPostFrameCallback(_afterLayout);
   }
 
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).brightness == Brightness.light
-        ? _lightTheme
-        : _darkTheme;
-    final hour =
-        DateFormat(widget.model.is24HourFormat ? 'HH' : 'hh').format(_dateTime);
-    final minute = DateFormat('mm').format(_dateTime);
-    final fontSize = MediaQuery.of(context).size.width / 3.5;
-    final offset = -fontSize / 7;
-    final defaultStyle = TextStyle(
-      color: colors[_Element.text],
-      fontFamily: 'PressStart2P',
-      fontSize: fontSize,
-      shadows: [
-        Shadow(
-          blurRadius: 0,
-          color: colors[_Element.shadow],
-          offset: Offset(10, 0),
-        ),
-      ],
+    final time = DateFormat.Hms().format(DateTime.now());
+    final weatherInfo = DefaultTextStyle(
+      style: TextStyle(color: Colors.red),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(_temperature),
+          Text(_temperatureRange),
+          Text(_condition),
+          Text(_location),
+        ],
+      ),
     );
 
-    return Container(
-      color: colors[_Element.background],
-      child: Center(
-        child: DefaultTextStyle(
-          style: defaultStyle,
+    double clockSize = _clockBoxSize.height / 100 * 50;
+
+    print(clockSize);
+    return Semantics.fromProperties(
+      key: _clockBoxKey,
+      properties: SemanticsProperties(
+        label: 'Analog clock with time $time',
+        value: time,
+      ),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: _clockBoxSize.width,
+            height: _clockBoxSize.height,
+            color: Theme.of(context).brightness == Brightness.light
+                ? Colors.white
+                : Colors.black87,
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: weatherInfo,
+                  ),
+                ),
+                ClockNumbers(fontSize: clockSize,)
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ClockNumbers extends StatelessWidget {
+  final int clockNumber;
+  final double fontSize;
+
+  ClockNumbers({
+    Key key,
+    this.fontSize,
+    this.clockNumber,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext buildContext) {
+    final Shader linearGradient = LinearGradient(
+      colors: <Color>[Color(0xff8921aa), Color(0xffDA44bb)],
+    ).createShader(Rect.fromCircle(center: Offset(0, 0), radius: 300));
+
+    List<Widget> _number3DEffect() {
+      List<Widget> _list = [];
+      for (var number = 0; number < 40; number++)
+        _list.add(Align(
+          alignment: Alignment.topCenter,
           child: Stack(
             children: <Widget>[
-              Positioned(left: offset, top: 0, child: Text(hour)),
-              Positioned(right: offset, bottom: offset, child: Text(minute)),
+              Positioned(
+                top: (1 * number).toDouble(),
+                left: (1 * number).toDouble(),
+                child: Text(
+                  55.toString(),
+                  style: TextStyle(
+                    fontFamily: "ubuntu_bold_italic",
+                    fontSize: fontSize,
+                    foreground:
+                        number != 0 ? (Paint()..shader = linearGradient) : null,
+                    color: number == 0 ? Colors.white : null,
+                  ),
+                ),
+              ),
             ],
           ),
-        ),
+        ));
+
+      _list = _list.reversed.toList();
+
+      return _list;
+    }
+
+    return Container(
+      child: Stack(
+        alignment: Alignment.center,
+        children: _number3DEffect(),
       ),
     );
   }
